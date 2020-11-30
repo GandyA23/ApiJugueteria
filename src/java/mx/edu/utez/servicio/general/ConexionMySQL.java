259929e7182c0823084d66337e2178582d19b853
GandyA23 <avila.gandy@gmail.com>
2020-11-30 11:17:51 -0600

    Cambio de ruta de los archivos de conexión a la base de datos

--- src/java/mx/edu/utez/servicio/general/ConexionMySQL.java
+++ src/java/mx/edu/utez/servicio/general/ConexionMySQL.java
@@ -5,16 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionMySQL {
-
     public static Connection getConexion() throws SQLException {
         Connection con;
         String driver = "com.mysql.jdbc.Driver";
 
         //Conexión a localhost
-        String user = "root";
-        String pass = "Avila-23";
         String db = "piplux";
         String url = "jdbc:mysql://localhost/"+db+"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        String user = "root";
+        String pass = "Avila-23";
 
         System.setProperty(driver,"");
         try{
@@ -27,14 +26,9 @@ public class ConexionMySQL {
 
     }
 
-    public static void main(String[] args) {
-        try {
-            ConexionMySQL c = new ConexionMySQL();
-            Connection con = c.getConexion();
-            System.out.println(con);
-            System.out.println("(^_^) Connection successful ... ");
-        } catch (Exception e) {
-            System.out.println("(o_O) Conection error ... " + e);
-        }
+
+    public static void main(String[] args) throws SQLException {
+        ConexionMySQL c = new ConexionMySQL();
+        System.out.println(c.getConexion());
     }
 }
